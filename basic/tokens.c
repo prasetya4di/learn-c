dfe9a955ddcfe70ec05b9da1244213f7f34207fa
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-06 20:16:59 +0700

    Add tokens.c

---
+++ basic/tokens.c
@@ -0,0 +1,28 @@
+//
+// Created by Pras Adi on 06/10/22.
+//
+
+int main() {
+
+    /*
+     * This is an example of variable declare in C
+     * int, double are keywords
+     * amount, total balance are keywords
+     */
+    int amount;
+    double totalbalance;
+
+    /*
+     * You can also declare a variable like this
+     * There are 32 reserved keywords in C
+     * auto, break, case, char, const, continue,
+     * default, do, double, else, enum, extern, float,
+     * for, goto, if, int, long, register, return,
+     * short, signed, sizeof, static, struct, switch,
+     * typedef, union, unsigned, void, volatile, while.
+     * !! Don't use keyword as identifier !!
+     */
+    float a, b;
+
+    return 0;
+}
